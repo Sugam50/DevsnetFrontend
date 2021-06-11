80af01f7c6aaeeed2365c6cad7746042305eb25f
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-06-12 03:14:11 +0530

    Update q5.js

--- DAY 6/q5.js
+++ DAY 6/q5.js
@@ -1,27 +1,17 @@
-function count(arr1){
-    var c=0
-    arr1.forEach(a=> {
-        if(a==='a'){
-            c=c+1
-        }
-     
-    }); 
-    return c
-}
 var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
-
-console.log("a ("+ count(arr1)+" times)");var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 var dict = {};
 
-for(i in arr1){
-    var temp = 0
-    for(j in arr1){
-        if(arr1[i]===arr1[j]){
-        temp += 1
+arr1.forEach(item1 => {
+
+    var temp=0
+    arr1.forEach(item2 => {
+        if(item1===item2){
+            temp=temp+1;
         }
-    }
-    dict[temp] = arr1[i];
-}
+    });
+    dict[temp] = item1;
+    
+});
 
 var max = 0;
 for(var key in dict) {
