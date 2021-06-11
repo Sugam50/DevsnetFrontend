95969cda40362d2479c116760861a3a98812a520
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-06-12 02:59:17 +0530

    Update q5.js

--- DAY 6/q5.js
+++ DAY 6/q5.js
@@ -10,4 +10,23 @@ function count(arr1){
 }
 var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 
-console.log("a ("+ count(arr1)+" times)");
+console.log("a ("+ count(arr1)+" times)");var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var dict = {};
+
+for(i in arr1){
+    var temp = 0
+    for(j in arr1){
+        if(arr1[i]===arr1[j]){
+        temp += 1
+        }
+    }
+    dict[temp] = arr1[i];
+}
+
+var max = 0;
+for(var key in dict) {
+    if (key > max){
+        max = key
+    }
+}
+console.log(dict[max] + "(" + max + "times)")
