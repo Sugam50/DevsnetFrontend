59e2cf714dea1197ee7ee732c432ce92fda0eb2c
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-06-12 00:16:03 +0530

    Add files via upload

---
+++ DAY 6/q4.js
@@ -0,0 +1,8 @@
+var myColor = ["Red", "Green", "White", "Black"];
+
+let str = myColor.join(',')
+let str2 = myColor.join(',')
+let str3 = myColor.join('+')
+console.log(str)
+console.log(str2)
+console.log(str3)
