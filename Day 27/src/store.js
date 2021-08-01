5359afd4bdebcd425d379e40f5ca0de67d795edb
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-08-01 14:18:14 +0530

    redux todo

---
+++ Day 27/src/store.js
@@ -0,0 +1,6 @@
+import rootReducer from "./reducers";
+import { applyMiddleware, createStore } from "redux";
+import logger from 'redux-logger';
+
+const store=createStore(rootReducer ,applyMiddleware(logger));
+export default store;
